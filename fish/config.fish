321b49447fe99ad63f2116153fde50c90b3a93ef
rixVe <piotr.litewka.2006@gmail.com>
2026-04-30 12:47:48 +0200

    add lua hyprland change hpyrlock change fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,21 +4,13 @@ end
 set fish_greeting
 alias v="nvim"
 alias hx="helix"
-alias extract="sh extract.sh"
 
 export EDITOR="helix"
 export SHELL="/bin/fish"
-
-export PF_COL1=3
-export PF_COL2=8
-export PF_COL3=7
-# export PF_INFO="ascii title os kernel host wm de uptime pkgs memory editor shell palette"
-export PF_INFO="title os kernel host wm de uptime pkgs memory editor shell palette"
-export PF_ALIGN=8
-export PF_SEP=""
 
 function fish_mode_prompt
 end
 export TERM="xterm-256color"
 fish_vi_key_bindings
 alias gittree="git log --all --topo-order --graph --show-signature"
+fzf --fish | source
